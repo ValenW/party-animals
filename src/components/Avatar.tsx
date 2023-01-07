8dc983d14d071080498bca23431bda53265d7d18
Valen Wang <valen@valen.wang>
2023-01-07 17:53:21 +0800

    feat: impl active animation

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import AvatarActive from "../assets/avatars/avatar_active.png";
 import classnames from "classnames";
+import { motion } from "framer-motion";
+
 import "./Avatar.css";
 
 interface Props {
@@ -11,14 +13,31 @@ interface Props {
 
 export const Avatar: FC<Props> = ({ imgPath, active, onClick }) => {
   return (
-    <div
+    <motion.div
       className={classnames("list-item", { active })}
       onClick={() => onClick?.(imgPath)}
+      animate={{
+        scale: active ? 1.3 : 1,
+        marginLeft: active ? 12 : 0,
+        marginRight: active ? 12 : 0,
+      }}
+      transition={{
+        scale: {
+          type: "spring",
+          bounce: active ? 0 : 0.5,
+          duration: 0.8,
+        },
+        default: { type: "tween" },
+      }}
     >
-      <div className="avatar-wrapper">
-        <img src={AvatarActive} className="active-img" />
+      <motion.div className="avatar-wrapper">
+        <motion.img
+          src={AvatarActive}
+          className="active-img"
+          animate={{ scale: active ? 1.2 : 1 }}
+        />
         <img src={imgPath} className="avatar-img" />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
